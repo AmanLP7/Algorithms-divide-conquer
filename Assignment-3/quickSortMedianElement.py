2c593e409def953856035cfe4a8715a082eaadfd
Aman Yadav <ak547276@gmail.com>
2020-05-18 15:21:37 +0530

    Made changes in the code.

--- Assignment-3/quickSortMedianElement.py
+++ Assignment-3/quickSortMedianElement.py
@@ -1,3 +1,6 @@
+# Importing numpy module
+from numpy import median
+
 # Importing text file data
 numbers = []
 with open('QuickSort.txt', 'r') as IntegerArray:
@@ -5,13 +8,28 @@ with open('QuickSort.txt', 'r') as IntegerArray:
     numbers.append(int(number.strip()))
 
 # Function to perform quicksort swaps given an array
+# using median of first, last and middle element as pivot.
 '''
 Input: Array, start index, length of array
 Output: Sorted array, number of comparisons and index of split
 '''
 def quickSortSwaps(array, firstIndex, lengthOfArray):
 
-  array[firstIndex], array[lengthOfArray - 1] = array[lengthOfArray - 1], array[firstIndex]
+  # Find the middle index of the array
+  if lengthOfArray % 2 == 0:
+    middleIndex = int(lengthOfArray/2) - 1
+  else:
+    middleIndex = lengthOfArray // 2
+
+  # Find the median element amongst first, last and middle element
+  medianElement = median([array[firstIndex], array[middleIndex], array[-1]])
+
+  # Swap first element with the pivot element
+  if medianElement == array[middleIndex]:
+    array[firstIndex], array[middleIndex] = array[middleIndex], array[firstIndex]
+  elif medianElement == array[-1]:
+    array[firstIndex], array[lengthOfArray - 1] = array[lengthOfArray - 1], array[firstIndex]
+
   pivotElement = array[firstIndex]
   partitionBoundary = firstIndex + 1
 
